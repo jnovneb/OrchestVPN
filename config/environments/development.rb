179d266f57cb341992f79be4f661b9066662db43
jnovneb <javier.novella.nebot@gmail.com>
2023-04-16 16:06:57 +0200

    Fix align center and style

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action.mailer.default_url_options = {:host => "dev.myhost.com"}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
