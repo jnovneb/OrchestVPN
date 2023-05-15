6590ced14976e98d390c2a218e459ef00d21908e
jnovneb <javier.novella.nebot@gmail.com>
2023-05-15 22:50:07 +0200

    Fixed some views

--- app/controllers/vpn_controller.rb
+++ app/controllers/vpn_controller.rb
@@ -3,4 +3,24 @@ class VpnController < ApplicationController
   def index
     render
   end
+
+  def new
+    @vpn = VPN.new
+  end
+
+  # Agrega la acción 'create' para manejar el envío del formulario
+  def create
+    @vpn = VPN.new(vpn_params)
+    if @vpn.save
+      redirect_to @vpn, notice: 'VPN creada exitosamente.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def vpn_params
+    params.require(:vpn).permit(:name, :servidor, :port, )
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_161154) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_190400) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,6 +22,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_161154) do
     t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "vpns", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "protocol"
+    t.integer "port"
+    t.string "server"
+    t.text "options"
+    t.string "technology"
+    t.string "VPNAdminList"
+    t.string "version"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
