921fb9311eaad5203409a6c28758ce1799d8281d
jnovneb <javier.novella.nebot@gmail.com>
2023-06-05 19:38:20 +0200

    Manage errors downloding ovpn.file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,14 +25,27 @@ class HomeController < ApplicationController
 
   def connect
     vpn_id = params[:vpn_id]
-    @vpn = Vpn.find(vpn_id)
-    # Guarda el nombre de la VPN y luego la ruta del archivo de configuración
-    nombre = @vpn.name
-    ruta = Rails.root.join('vpn_files', "#{nombre}", "#{nombre}.ovpn").to_s
-    # Envía el archivo como descarga adjunta
-    send_file(ruta, disposition: 'attachment', filename: "#{nombre}.ovpn")
-    
-    flash[:notice] = "Archivo descargado exitosamente"
-    redirect_to root_path
-  end
+    begin
+      @vpn = Vpn.find(vpn_id)
+      # Guarda el nombre de la VPN y luego la ruta del archivo de configuración
+      nombre = @vpn.name
+      ruta = Rails.root.join('vpn_files', "#{nombre}", "#{nombre}.ovpn").to_s
+      # Verifica si el archivo existe antes de enviarlo como descarga adjunta
+      if File.exist?(ruta)
+        # Envía el archivo como descarga adjunta
+        send_file(ruta, disposition: 'attachment', filename: "#{nombre}.ovpn")
+        flash[:notice] = "Archivo descargado exitosamente"
+        redirect_to root_path
+      else
+        flash[:error] = "El archivo de configuración no existe"
+        redirect_to root_path
+      end
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "VPN no encontrada"
+      redirect_to root_path
+    rescue => e
+      flash[:error] = "Ocurrió un error al descargar el archivo de configuración: #{e.message}"
+      redirect_to root_path
+    end
+  end 
 end
